9f52fdbb2f32cfa283238afd2c66bd5f87437144
alexannmill <alexannmill@gmail.com>
2022-08-31 21:21:05 +0000

    export connect funtion to client file, added .write name

--- client.js
+++ client.js
@@ -1,9 +1,31 @@
 const net = require("net");
+
+
 const connect = () => {
+  console.log("connecting...");
   const conn = net.createConnection({
      host: "localhost",
     port: 50541,
   });
+
+  conn.setEncoding("utf8");
+  
+  conn.on("connect", () => {
+    console.log("connection made")
+    
+    conn.write( "Name: LEX")
+  });
+
+
+  conn.on("data", (data) => {
+    console.log( "server:", data)
+  });
+
+
+  return conn;
+
 };
 
-module.exports = { connect };
+
+
+module.exports = { connect } ;
--- play.js
+++ play.js
@@ -1,27 +1,6 @@
-const net = require("net");
-// const conn = require("./client")
-
-const connect = () => {
-  const conn = net.createConnection({
-     host: "localhost",
-    port: 50541,
-  });
-  
-  
-  conn.setEncoding("utf8");
-  
-  conn.on("connect", () => {
-    console.log("connection made")
-  });
-  
-  conn.on("data", (data) => {
-    console.log( "server:", data)
-  })
-  
-};
+const { connect } = require("./client")
 
 
-// return conn;
 
-console.log("connecting...");
+
 connect();
